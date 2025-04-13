a85719bf7d29d17a73c2eb878de5840919bfa9fd
LucasRP <rodrigues.lrp21@gmail.com>
2025-04-13 16:07:16 -0300

    Iniciando Desenvolvimento da view e controller da operacao saque

--- telaSaque/TelaSaqueView.java
+++ telaSaque/TelaSaqueView.java
@@ -1,4 +1,23 @@
 package telaSaque;
 
-public class TelaSaqueView {
+import model.Model;
+import model.Observer;
+
+public class TelaSaqueView implements Observer {
+    private Model model;
+    private TelaSaqueViewController telaSaqueViewController;
+
+    public void initTelaSaqueview(Model model) {
+        this.model = model;
+        model.attachObserver(this);
+        telaSaqueViewController = new TelaSaqueViewController();
+        telaSaqueViewController.initTelaSaqueViewController(model, this);
+    }
+
+
+
+    @Override
+    public void update() {
+
+    }
 }
--- telaSaque/TelaSaqueViewController.java
+++ telaSaque/TelaSaqueViewController.java
@@ -1,4 +1,21 @@
 package telaSaque;
 
-public class TelaSaqueViewController {
+import model.Model;
+import model.Observer;
+
+public class TelaSaqueViewController implements Observer {
+    private Model model;
+    private TelaSaqueView telaSaqueView;
+
+    public void initTelaSaqueViewController(Model model, TelaSaqueView telaSaqueView) {
+        this.model = model;
+        this.telaSaqueView = telaSaqueView;
+        model.attachObserver(this);
+    }
+
+
+    @Override
+    public void update() {
+
+    }
 }
